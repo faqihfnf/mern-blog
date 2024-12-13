d7a4ade3565f4ce09e264b6d79a83ad4452dfa91
faqihfnf <faqih.fnf@gmail.com>
2024-12-14 01:32:23 +0700

    create signup api routes

---
+++ api/controllers/auth.controller.js
@@ -0,0 +1,26 @@
+import User from "../models/user.model.js";
+import bcrypt from "bcryptjs";
+
+export const signup = async (req, res) => {
+  const { username, email, password } = req.body;
+
+  //# pengecekan apakah ada field yang kosong saat user signup
+  if (!username || !email || !password || username === "" || email === "" || password === "") {
+    return res.status(400).json({ message: "All fields are required" });
+  }
+
+  const hashedPassword = bcrypt.hashSync(password, 10);
+
+  const newUser = new User({
+    username,
+    email,
+    password: hashedPassword,
+  });
+
+  try {
+    await newUser.save();
+    res.json({ message: "Signup successfully" });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRoute from "./routes/user.route.js";
+import authRoute from "./routes/auth.route.js";
 
 dotenv.config();
 
@@ -16,6 +17,10 @@ mongoose
 
 const app = express();
 
+//# digunakan agar backend bisa menerima data/inputan dalam bentuk json
+app.use(express.json());
+
 app.listen(3000, () => console.log("Server is running on port 3000"));
 
 app.use("/api/user", userRoute);
+app.use("/api/auth", authRoute);
