a3d6b3512039ba187befea6ceaba47f9ab7dd3f4
faqihfnf <faqih.fnf@gmail.com>
2024-12-19 02:09:22 +0700

    add update user api route

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRoute from "./routes/user.route.js";
 import authRoute from "./routes/auth.route.js";
+import cookieParser from "cookie-parser";
 
 dotenv.config();
 
@@ -19,6 +20,7 @@ const app = express();
 
 //# digunakan agar backend bisa menerima data/inputan dalam bentuk json
 app.use(express.json());
+app.use(cookieParser());
 
 app.listen(3000, () => console.log("Server is running on port 3000"));
 
